b47b56097e7b675a1532fa8c2428baf860b213af
t-riku <dingariku@icloud.com>
2022-04-23 15:55:08 +0900

    indent all update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Header from './Header';
-import Magazine from './routes/magazine';
-import Philosophy from './routes/philosophy';
-import Work from './routes/work';
-import Detail from './routes/detail';
-import Home from './routes/home';
-import Profile from './routes/profile';
-import Overview from './routes/overview';
-import Flow from './routes/flow';
-import Footer from './Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Magazine from "./routes/magazine";
+import Philosophy from "./routes/philosophy";
+import Work from "./routes/work";
+import Detail from "./routes/detail";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Overview from "./routes/overview";
+import Flow from "./routes/flow";
+import Footer from "./Footer";
 import db from "./firebase";
 import { doc, collection, getDocs, onSnapshot } from "firebase/firestore";
 
-
 function App() {
-
   const [workMap, setWorkMap] = useState(new Map());
 
   useEffect(() => {
@@ -28,10 +22,10 @@ function App() {
     getDocs(workData).then((snapShot) => {
       // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
       const workMap = new Map();
-      snapShot.docs.forEach(doc => {
+      snapShot.docs.forEach((doc) => {
         const work = doc.data();
         work.path = doc.id;
-        workMap.set(work.path, work)
+        workMap.set(work.path, work);
       });
       setWorkMap(workMap);
     });
@@ -39,31 +33,42 @@ function App() {
     // リアルタイムで取得
     onSnapshot(workData, (work) => {
       const workMap = new Map();
-      workData.docs.forEach(doc => {
+      workData.docs.forEach((doc) => {
         const work = doc.data();
         work.path = doc.id;
-        workMap.set(work.path, work)
+        workMap.set(work.path, work);
       });
       setWorkMap(workMap);
-    })
+    });
   }, []);
 
   return (
     <>
-      <Header/>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="philosophy" element={<Philosophy />} />
-            <Route path="work" element={<Work works={Array.from(workMap).map(([_, work])=> work)}/>} />
-            {Array.from(workMap).map(([path, work]) => <Route key={`${path}`} path={`work/${path}`} element={<Detail work={work}/>}/>)}
-            <Route path="magazine" element={<Magazine />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="overview" element={<Overview />} />
-            <Route path="flow" element={<Flow />} />
-          </Routes>
-        </BrowserRouter>
-      <Footer/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="philosophy" element={<Philosophy />} />
+          <Route
+            path="work"
+            element={
+              <Work works={Array.from(workMap).map(([_, work]) => work)} />
+            }
+          />
+          {Array.from(workMap).map(([path, work]) => (
+            <Route
+              key={`${path}`}
+              path={`work/${path}`}
+              element={<Detail work={work} />}
+            />
+          ))}
+          <Route path="magazine" element={<Magazine />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="overview" element={<Overview />} />
+          <Route path="flow" element={<Flow />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
   );
 }
--- src/routes/work.jsx
+++ src/routes/work.jsx
@@ -1,64 +1,70 @@
 export default function Work(props) {
-    const works = props.works;
-    return(
-        <main>
-        <div className="title-box work-width">
-            <h1 className="work-margin">宿泊施設</h1>
-                <div className="work-cards">
-                {works.map((work => work.type === "accommodation" ? (
-                    <div className="work-card">
-                        <div className="work-box">
-                            <img src={work.thumbnailImageUrl} alt=""/>
-                            <div className="mask">
-                                <a href={`/work/${work.path}`}>
-                                    <div className="caption">
-                                        {work.caption}
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ): <></>))}
+  const works = props.works;
+  return (
+    <main>
+      <div className="title-box work-width">
+        <h1 className="work-margin">宿泊施設</h1>
+        <div className="work-cards">
+          {works.map((work) =>
+            work.type === "accommodation" ? (
+              <div className="work-card">
+                <div className="work-box">
+                  <img src={work.thumbnailImageUrl} alt="" />
+                  <div className="mask">
+                    <a href={`/work/${work.path}`}>
+                      <div className="caption">{work.caption}</div>
+                    </a>
+                  </div>
                 </div>
-            </div>
-        <div class="title-box work-width">
-            <h1 class="work-margin">住宅</h1>
-                <div className="work-cards">
-                {works.map((work => work.type === "house" ? (
-                    <div className="work-card">
-                        <div className="work-box">
-                            <img src={work.thumbnailImageUrl} alt=""/>
-                            <div className="mask">
-                                <a href={`/work/${work.path}`}>
-                                    <div className="caption">
-                                        {work.caption}
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ): <></>))}
-                </div>
-            </div>
-        <div class="title-box work-width">
-            <h1 class="work-margin">デザイン</h1>
-            <div className="work-cards">
-                {works.map((work => work.type === "design" ? (
-                    <div className="work-card">
-                        <div className="work-box">
-                            <img src={work.thumbnailImageUrl} alt=""/>
-                            <div className="mask">
-                                <a href={`/work/${work.path}`}>
-                                    <div className="caption">
-                                        {work.caption}
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ): <></>))}
-                </div>
+              </div>
+            ) : (
+              <></>
+            )
+          )}
         </div>
+      </div>
+      <div class="title-box work-width">
+        <h1 class="work-margin">住宅</h1>
+        <div className="work-cards">
+          {works.map((work) =>
+            work.type === "house" ? (
+              <div className="work-card">
+                <div className="work-box">
+                  <img src={work.thumbnailImageUrl} alt="" />
+                  <div className="mask">
+                    <a href={`/work/${work.path}`}>
+                      <div className="caption">{work.caption}</div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )
+          )}
+        </div>
+      </div>
+      <div class="title-box work-width">
+        <h1 class="work-margin">デザイン</h1>
+        <div className="work-cards">
+          {works.map((work) =>
+            work.type === "design" ? (
+              <div className="work-card">
+                <div className="work-box">
+                  <img src={work.thumbnailImageUrl} alt="" />
+                  <div className="mask">
+                    <a href={`/work/${work.path}`}>
+                      <div className="caption">{work.caption}</div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )
+          )}
+        </div>
+      </div>
     </main>
-    );
+  );
 }
